06db61205bdc2d174d63bdd29c59a04571113fa6
Šprinc Jan <sprinc@spstrutnov.cz>
2021-10-23 15:56:29 +0200

    Editování článků

--- src/app/models/dbModel.js
+++ src/app/models/dbModel.js
@@ -10,3 +10,15 @@ exports.nacistVse = () => {
     return db.JSON();
 }
 
+
+exports.editArticle = (id, items) => {
+    var Article = db.get(id);
+    Article.autor = (items?.autor===undefined?Article.autor:items.autor);
+    Article.datum = (items?.datum===undefined?Article.datum:items.datum);
+    Article.viditelny = (items?.viditelny===undefined?Article.viditelny:items.viditelny);
+    Article.nadpis = (items?.nadpis===undefined?Article.nadpis:items.nadpis);
+    Article.popis_short = (items?.popis_short===undefined?Article.popis_short:items.popis_short);
+    Article.popis_full = (items?.popis_full===undefined?Article.popis_full:items.popis_full);
+    Article.tagy = (items?.tagy===undefined?Article.tagy:items.tagy);
+    db.set(id, Article);
+}
