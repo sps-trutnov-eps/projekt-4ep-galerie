a83cad6294f97006df4106458ce18677f5949700
pchalek <pchalek@spstrutnov.cz>
2021-12-09 21:34:53 +0100

    Příprava pro Session

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,25 @@
+const { strict } = require('assert/strict');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const app = express();
+const key = require('dotenv').config();
+
+app.use('/admin', session({
+    resave: false,
+    secure: false,
+    saveUninitialized: false,
+    secret: key,
+    cookie: {
+        sameSite: 'strict',
+        expires: 600000,
+    }
+}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'www')));
 app.use(express.json());
-app.use('/admin', session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'pepe',
-    cookie: {
-        expires: 600000
-    }
-}));
-
 app.use(express.urlencoded({ "extended": true }));
 app.use('/', require(path.join(__dirname, 'routers', 'dbRouter')));
 module.exports = app;
