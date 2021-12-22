f73cfabefcbd12881dbd1d7dc8ec9af7ec790526
Šprinc Jan <sprinc@spstrutnov.cz>
2021-12-22 22:32:14 +0100

    Upravení pravomocí

--- src/app/routers/dbRouter.js
+++ src/app/routers/dbRouter.js
@@ -4,22 +4,20 @@ const controller = require(path.join(__dirname, '..', 'controllers', 'dbControll
 const router = express.Router();
 const verify = require('../middleware/admin-auth')
 
-router.get('/admin/upload', controller.upload);
-router.get('/admin/test', controller.upload);
+router.get('/admin/upload', verify, controller.upload);
 router.get('/admin',controller.admin);
-router.get('/admin/getArticleNames', controller.getArticleNames);
+router.get('/admin/getArticleNames', verify, controller.getArticleNames);
 router.get('/admin/edit/:article',verify, controller.getArticleData)
 router.get('/admin/edit', verify, controller.adminEdit);
-router.get('/admin/getArticleTitles', controller.getArticleTitles);
+router.get('/admin/getArticleTitles', verify, controller.getArticleTitles);
 router.get('/admin/compare', controller.compareAdmin);
 router.get('/projekty/detail/:id', controller.detail);
 
-router.post('/newArticle',controller.uploadArticle);
+router.post('/newArticle', verify, controller.uploadArticle);
 router.post('/admin/sendImg',controller.pre_upload ,controller.uploadImg, controller.uploadArticle);
-router.post('/admin/editArticle', controller.editArticle);
-router.post('/admin/deleteArticle', controller.deleteArticle);
+router.post('/admin/editArticle', verify, controller.editArticle);
+router.post('/admin/deleteArticle', verify, controller.deleteArticle);
 
-router.post('/newArticle',controller.uploadArticle);
 router.post('/admin/loginInfo', controller.postLoginInfo);
 router.post('/admin/logout', controller.logout)
 
