8092c7f4c0dab04676dc930860c1ed1d77af40fa
Jakub Šenkýř <senkyr@spstrutnov.cz>
2022-01-17 08:41:20 +0100

    Model reflektuje přejmenování controllerů

---
+++ src/app/models/projektyModel.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const JSONdb = require('simple-json-db');
+const db_tagy = new JSONdb(path.join(__dirname, '..', '..', '..', 'data', 'tagy.json'));
+const db_projekty = new JSONdb(path.join(__dirname, '..', '..', '..', 'data', 'clanky.json'));
+
+exports.Ziskej_tagy = () => {
+    return db_tagy.JSON();
+}
+
+exports.Pocet_projektu = () => {
+    return db_projekty.JSON().length;
+}
